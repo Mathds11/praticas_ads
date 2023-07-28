8931dc84c29250c7a4cea37d099daff5ac94817b
Matheus <matheus.da.silvajk@gmail.com>
2023-07-27 23:55:00 -0400

    nome com *

--- Curso_Python/while.py
+++ Curso_Python/while.py
@@ -1,15 +1,27 @@
-linha = 5
-coluna = 5
+# linha = 5
+# coluna = 5
 
-n = 1
-while n <= linha:
-    x = 1
-    print()
-    print(f'{n}ª linha: ', end='')
-    while x <= coluna:
-        print( x, end='')
-        x += 1
+# n = 1
+# while n <= linha:
+#     x = 1
+#     print()
+#     print(f'{n}ª linha: ', end='')
+#     while x <= coluna:
+#         print( x, end='')
+#         x += 1
+#     n += 1
+
+
+# print('\nFim')
+
+nome = 'Matheus'
+
+n = 0
+novo_nome = ''
+while n < len(nome):
+    novo_nome += f'*{nome[n]}'
     n += 1
 
 
-print('\nFim')
+novo_nome += '*'
+print(novo_nome)
